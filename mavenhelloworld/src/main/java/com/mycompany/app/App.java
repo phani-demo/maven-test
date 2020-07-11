71f256167b3dae86d08889088d8d1f6a26009883
phani-demo <66237529+phani-demo@users.noreply.github.com>
2020-07-11 21:36:00 +0530

    Update App.java

--- mavenhelloworld/src/main/java/com/mycompany/app/App.java
+++ mavenhelloworld/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String args[])
     {
-        System.out.println( "Hello World! 8.10pm 11 july.. for polling check" );
+        System.out.println( "Hello World! 9.35pm 11 july." );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
