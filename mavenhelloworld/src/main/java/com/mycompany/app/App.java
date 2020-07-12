b20ae42b83ca7a6425f5266bb901ee6c76fcd5b7
pcherugh <pcherugh@LIN66006118.corp.capgemini.com>
2020-07-12 17:32:59 +0530

    5.32pm 12 july

--- mavenhelloworld/src/main/java/com/mycompany/app/App.java
+++ mavenhelloworld/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String args[])
     {
-        System.out.println( "Hello World! 1.15pm 12 july" );
+        System.out.println( "Hello World! 5.32pm 12 july" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
